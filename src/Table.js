dfc3a954f479d5156cb852805e2dca9b58b73160
Sandhya Mahat <33580255+Sannu8@users.noreply.github.com>
2017-11-22 03:04:09 +0200

    Update Table.js

--- src/Table.js
+++ src/Table.js
@@ -1,49 +1,108 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
-/*import edit from './Edit.png';
-import trash from './Trash.png';*/
+
 import './Nord.css';
-import App from './App'
 
-const row = (x, i, header) => 
+
+
+const row = (
+    x, 
+    i, 
+    header, 
+    handleRemove, 
+    startEditing, 
+    editIdx, 
+    handleChange,
+    stopEditing
+) => { 
+   const currentlyEditing = editIdx ===i;
+    
+    return (
     <tr className= "Data-Row" key = {'tr-${i}'}>
-        {header.map((y, k) => 
+        {header.map((y, k) => (
         <td key = {'td-${k}'}> 
-            {x[y.prop]} 
+            {currentlyEditing ? (
+                <input type="text" 
+                    name={y.prop} 
+                    onChange ={e => handleChange(e, y.prop, i)} 
+                    value = {x[y.prop]} 
+        />
+                ) : (
+                    x[y.prop] 
+                    )}
         </td>
+        ))}
+    
+        <td>
+        {currentlyEditing ? (
+         <input type="button" value="Cancel" className="CancelBtn" onClick={() => stopEditing()} />
+         
+         ) : (
+         
+            <span onClick = {() => startEditing(i)}>  
+        
+            <svg fill="#909090" height="24" viewBox="0 0 24 24" width="24" className="edit" alt="edit" >
+                <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+                <path d="M0 0h24v24H0z" fill="none"/>
+        
+            </svg>
+            </span>
         )}
-    <td>
-    <svg fill="#909090" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" className="edit" alt="edit" >
-    <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
-    <path d="M0 0h24v24H0z" fill="none"/>
-</svg>
-             <svg fill="#909090" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" className= "removeData" alt="trash"
-               >
-    <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
-    <path d="M0 0h24v24H0z" fill="none"/>
-                    </svg>
     </td>
-    </tr>;
-
-export default ({data, header}) => 
+   
+    <td>
+        {currentlyEditing ? (
+        <input type="button" value="Save" className="SaveBtn" onClick={() => stopEditing()} />
+        
+        ) : (
+            
+        <span onClick = {() => handleRemove(i)}>
+             <svg fill="#909090" height="24" viewBox="0 0 24 24" width="24" className= "removeData" alt="trash">
+                <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
+                <path d="M0 0h24v24H0z" fill="none"/>
+            </svg>
+        </span>
+         )}       
+    </td>
+    </tr>
+    );
+}
+export default ({data, header, handleRemove, 
+    startEditing, 
+    editIdx, 
+    handleChange,
+    stopEditing}) => 
         
         <table classname="AddNew">
            
             <tr className="Row-title" >
-                {
-            header.map((x,i) =>
-                <th key= {'th-${i}'}> 
-                    
-                    {x.name}
-                </th>)
-}
+                {header.map((x,i) => (
+                    <th key= {'th-${i}'}> {x.name} </th>
+            ))}
+            <th />
+            <th />
             </tr>
             
-            {data.map((x, i) => row(x, i, header))}
+            {data.map((x, i) => 
+                      row(
+                x, 
+                i, 
+                header, 
+                handleRemove, 
+                startEditing, 
+                editIdx, 
+                handleChange,
+                stopEditing))}
         </table>
         
       
         
+    
+
+
+
+
+
     
 
 
